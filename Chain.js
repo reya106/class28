542d0f06d1d0ae283f54593a29c7e78baba9e579
reya106 <75875442+reya106@users.noreply.github.com>
2021-04-29 20:46:49 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,27 @@
+class Chain{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    display(){
+        if(this.chain.bodyA){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    fly(){
+        // make the bird free from the constraint
+        console.log("in fly function");
+        this.chain.bodyA=null
+
+    }
+}
